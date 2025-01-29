c99185f8566916d448a0d802bbf5a7c01e58c5d8
Nagayama Hiroki <16367098+ikorihn@users.noreply.github.com>
2025-01-29 21:00:19 +0900

    feat: open URL for Google Cloud console

--- src/google-cloud-console-opener.tsx
+++ src/google-cloud-console-opener.tsx
@@ -1,17 +1,73 @@
-import { ActionPanel, Detail, List, Action, Icon } from "@raycast/api";
+import { ActionPanel, Detail, List, Action, Icon, open } from "@raycast/api";
+
+// 全サービスじゃないしGoogle Cloudに追従しないといけない
+const consoleUrls = [
+  { service: "IAM", path: "iam-admin", icon: "" },
+  { service: "Compute Engine", path: "compute", icon: "" },
+  { service: "Cloud Storage", path: "storage", icon: "" },
+  { service: "BigQuery", path: "bigquery", icon: "" },
+  { service: "Pub/Sub", path: "cloudpubsub", icon: "" },
+  { service: "Dataflow", path: "dataflow", icon: "" },
+  { service: "VPC Network", path: "networking", icon: "" },
+  { service: "Cloud Run", path: "run", icon: "" },
+  { service: "Cloud Run functions", path: "functions", icon: "" },
+  { service: "Database Center", path: "database-center", icon: "" },
+  { service: "AlloyDB", path: "alloydb/clusters", icon: "" },
+  { service: "SQL", path: "sql/instances", icon: "" },
+  { service: "Firestore", path: "firestore/databases", icon: "" },
+  { service: "Logging", path: "logs", icon: "" },
+  { service: "Monitoring", path: "monitoring", icon: "" },
+  { service: "Error Reporting", path: "errors", icon: "" },
+  { service: "Trace", path: "traces", icon: "" },
+  { service: "Profiler", path: "profiler", icon: "" },
+  { service: "Risk overview", path: "security/command-center", icon: "" },
+  { service: "Cloud Build", path: "cloud-build", icon: "" },
+  { service: "Artifact Registry", path: "artifacts", icon: "" },
+  { service: "Vertex AI", path: "vertex-ai", icon: "" },
+  { service: "Cloud Scheduler", path: "cloudscheduler", icon: "" },
+  { service: "Eventarc", path: "eventarc/triggers", icon: "" },
+];
+
+// TODO projectsは外から設定する
+const projects = [""];
 
 export default function Command() {
   return (
     <List>
-      <List.Item
-        icon={Icon.Bird}
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+      {consoleUrls.map((item, index) => (
+        <List.Item
+          key={item.service}
+          icon={Icon.Bird}
+          title={item.service}
+          actions={
+            <ActionPanel>
+              <Action.Push title="Show Details" target={SelectProject(item.path)} />
+            </ActionPanel>
+          }
+        />
+      ))}
     </List>
   );
 }
+
+const SelectProject = (path: string) => {
+  return (
+    <List>
+      {projects.map((item, index) => (
+        <List.Item
+          key={item}
+          icon={Icon.Link}
+          title={item}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Jump to link"
+                onAction={() => open(`https://console.cloud.google.com/${path}?project=${item}`)}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+};
