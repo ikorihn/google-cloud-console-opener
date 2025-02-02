9fb8b34fba933edbd0d9cebf19cc43585747933d
Nagayama Hiroki <16367098+ikorihn@users.noreply.github.com>
2025-02-02 11:07:18 +0900

    show service icons

--- src/google-cloud-console-opener.tsx
+++ src/google-cloud-console-opener.tsx
@@ -2,30 +2,32 @@ import { ActionPanel, Detail, List, Action, Icon, open } from "@raycast/api";
 
 // 全サービスじゃないしGoogle Cloudに追従しないといけない
 const consoleUrls = [
-  { service: "IAM", path: "iam-admin", icon: "" },
-  { service: "Compute Engine", path: "compute", icon: "" },
-  { service: "Cloud Storage", path: "storage", icon: "" },
-  { service: "BigQuery", path: "bigquery", icon: "" },
-  { service: "Pub/Sub", path: "cloudpubsub", icon: "" },
-  { service: "Dataflow", path: "dataflow", icon: "" },
-  { service: "VPC Network", path: "networking", icon: "" },
-  { service: "Cloud Run", path: "run", icon: "" },
-  { service: "Cloud Run functions", path: "functions", icon: "" },
+  { service: "IAM", path: "iam-admin", icon: "google-cloud-icons/identity_and_access_management/identity_and_access_management.svg" },
+  { service: "Compute Engine", path: "compute", icon: "google-cloud-icons/compute_engine/compute_engine.svg" },
+  { service: "Cloud Storage", path: "storage", icon: "google-cloud-icons/cloud_storage/cloud_storage.svg" },
+  { service: "BigQuery", path: "bigquery", icon: "google-cloud-icons/bigquery/bigquery.svg" },
+  { service: "Pub/Sub", path: "cloudpubsub", icon: "google-cloud-icons/pubsub/pubsub.svg" },
+  { service: "Dataflow", path: "dataflow", icon: "google-cloud-icons/dataflow/dataflow.svg" },
+  { service: "VPC Network", path: "networking", icon: "google-cloud-icons/virtual_private_cloud/virtual_private_cloud.svg" },
+  { service: "Load balancing", path: "net-services/loadbalancing", icon: "google-cloud-icons/cloud_load_balancing/cloud_load_balancing.svg" },
+  { service: "Cloud DNS", path: "net-services/dns", icon: "google-cloud-icons/cloud_dns/cloud_dns.svg" },
+  { service: "Cloud Run", path: "run", icon: "google-cloud-icons/cloud_run/cloud_run.svg" },
+  { service: "Cloud Run functions", path: "functions", icon: "google-cloud-icons/cloud_functions/cloud_functions.svg" },
   { service: "Database Center", path: "database-center", icon: "" },
-  { service: "AlloyDB", path: "alloydb/clusters", icon: "" },
-  { service: "SQL", path: "sql/instances", icon: "" },
-  { service: "Firestore", path: "firestore/databases", icon: "" },
-  { service: "Logging", path: "logs", icon: "" },
-  { service: "Monitoring", path: "monitoring", icon: "" },
-  { service: "Error Reporting", path: "errors", icon: "" },
-  { service: "Trace", path: "traces", icon: "" },
-  { service: "Profiler", path: "profiler", icon: "" },
-  { service: "Risk overview", path: "security/command-center", icon: "" },
-  { service: "Cloud Build", path: "cloud-build", icon: "" },
-  { service: "Artifact Registry", path: "artifacts", icon: "" },
-  { service: "Vertex AI", path: "vertex-ai", icon: "" },
-  { service: "Cloud Scheduler", path: "cloudscheduler", icon: "" },
-  { service: "Eventarc", path: "eventarc/triggers", icon: "" },
+  { service: "AlloyDB", path: "alloydb/clusters", icon: "google-cloud-icons/alloydb.png" },
+  { service: "SQL", path: "sql/instances", icon: "google-cloud-icons/cloud_sql/cloud_sql.svg" },
+  { service: "Firestore", path: "firestore/databases", icon: "google-cloud-icons/firestore/firestore.svg" },
+  { service: "Logging", path: "logs", icon: "google-cloud-icons/cloud_logging/cloud_logging.svg" },
+  { service: "Monitoring", path: "monitoring", icon: "google-cloud-icons/cloud_monitoring/cloud_monitoring.svg" },
+  { service: "Error Reporting", path: "errors", icon: "google-cloud-icons/error_reporting/error_reporting.svg" },
+  { service: "Trace", path: "traces", icon: "google-cloud-icons/trace/trace.svg" },
+  { service: "Profiler", path: "profiler", icon: "google-cloud-icons/profiler/profiler.svg" },
+  { service: "Risk overview", path: "security/command-center", icon: "google-cloud-icons/security_command_center/security_command_center.svg" },
+  { service: "Cloud Build", path: "cloud-build", icon: "google-cloud-icons/cloud_build/cloud_build.svg" },
+  { service: "Artifact Registry", path: "artifacts", icon: "google-cloud-icons/artifact_registry/artifact_registry.svg" },
+  { service: "Vertex AI", path: "vertex-ai", icon: "google-cloud-icons/vertexai/vertexai.svg" },
+  { service: "Cloud Scheduler", path: "cloudscheduler", icon: "google-cloud-icons/cloud_scheduler/cloud_scheduler.svg" },
+  { service: "Eventarc", path: "eventarc/triggers", icon: "google-cloud-icons/eventarc/eventarc.svg" },
 ];
 
 // TODO projectsは外から設定する
@@ -37,7 +39,7 @@ export default function Command() {
       {consoleUrls.map((item, index) => (
         <List.Item
           key={item.service}
-          icon={Icon.Bird}
+          icon={item.icon || "google-cloud.svg"}
           title={item.service}
           actions={
             <ActionPanel>
